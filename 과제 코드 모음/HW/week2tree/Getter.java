747bce18d43ce00e2f28216cbe34557ffdff97f3
rain2473 <71944345+rain2473@users.noreply.github.com>
2023-03-19 19:21:48 +0900

    Fix : Fix Code

--- 과제 코드 모음/HW/week2tree/Getter.java
+++ 과제 코드 모음/HW/week2tree/Getter.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 // 인스턴스를 생성하면 안되므로, interface를 채택합니다.
 public interface Getter {
     // 스캐너를 main 메소드에서 선언하는 것은 cool하지 못하므로 이 인터페이스에서 선언하여 재사용합니다.
-    static final Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
     // 입력을 한줄씩 받아 문자열로 반환하는 메소드 입니다.
     public static String getLine() {
